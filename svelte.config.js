e891759ab7aa14ac1d3d21c75fc843c884a02fac
techa <techa27@gmail.com>
2022-09-11 01:22:01 +0900

    Revert "fix: `vite build` does not set process.env.NODE_ENV"
    This reverts commit 3e749fa0098a46c2e8689279c6a4e1edd67dcae6.

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import preprocess from 'svelte-preprocess'
 import { readFileSync } from 'fs'
 const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
