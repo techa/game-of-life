672952f5651486c932fd6f1d5067fcd241db0fe3
techa <techa27@gmail.com>
2023-11-18 17:15:57 +0900

    fix: Integrate Handle into Slider.

--- src/components/generic/Slider.svelte
+++ src/components/generic/Slider.svelte
@@ -2,7 +2,6 @@
 	import { createEventDispatcher } from 'svelte'
 	import { drag } from '../actions/drag'
 	import { writable, type Writable } from 'svelte/store'
-	import Handle from './Handle.svelte'
 
 	const dispatch = createEventDispatcher()
 	const dragging: Writable<Element | null> = writable(null)
@@ -14,7 +13,7 @@
 	export let min = 0
 	export let max = 100
 	export let step = 1
-	export let direction: 'horizontal' | 'vertical' = 'horizontal'
+	export let orientation: 'horizontal' | 'vertical' = 'horizontal'
 	export let reverse = false
 	export let gradient = ['white', 'black']
 
@@ -25,7 +24,7 @@
 	let turn = 0
 	let position = 0
 
-	$: vertical = direction === 'vertical'
+	$: vertical = orientation === 'vertical'
 
 	$: {
 		turn = 0 + (vertical ? 0 : -0.25) + (reverse ? 0 : 0.5)
@@ -61,14 +60,14 @@
 </script>
 
 <div
-	class="slider {direction} {$$props.class || ''}"
+	class="slider {orientation} {$$props.class || ''}"
 	style={$$props.style}
 	style:background={`linear-gradient(${turn}turn, ${gradient})`}
 	role="slider"
 	aria-valuenow={value}
 	aria-valuemin={min}
 	aria-valuemax={max}
-	aria-orientation={direction}
+	aria-orientation={orientation}
 	tabindex={-1}
 	bind:clientWidth={width}
 	bind:clientHeight={height}
@@ -76,14 +75,14 @@
 	on:drag={setValue}
 >
 	{#if handle}
-		<Handle
-			{direction}
-			{value}
-			{dragging}
-			digit={max === 1 && min === 0 ? 2 : 0}
-			bottom={vertical ? position : 0}
-			left={vertical ? 0 : position}
-		/>
+		<div
+			class="handle {orientation}"
+			class:dragging={$dragging}
+			style:bottom={`${vertical ? position : 0}px`}
+			style:left={`${vertical ? 0 : position}px`}
+		>
+			{+value.toFixed(max === 1 && min === 0 ? 2 : 0)}
+		</div>
 	{/if}
 </div>
 
@@ -101,5 +100,64 @@
 	.slider.horizontal {
 		height: var(--cell-size, 24px);
 		cursor: col-resize;
+	}
+
+	/* Handle -------- */
+
+	.handle {
+		--size: 14px;
+		width: var(--size);
+		height: var(--size);
+		margin: calc(var(--size) / -2);
+
+		position: absolute;
+		bottom: 0;
+		left: 0;
+
+		border-radius: calc(var(--size) / 2);
+
+		border: 1px solid var(--black);
+
+		color: transparent;
+		background-color: var(--background-color);
+
+		pointer-events: none;
+		user-select: none;
+
+		/* box-shadow:
+			0 0 2.5px 0 rgba(255, 255, 255, 0.5),
+			inset 0 0 2.5px 0 rgba(255, 255, 255, 0.5); */
+	}
+
+	.handle.vertical {
+		--size: 8px;
+		width: calc(100% + 2px);
+		height: var(--size);
+		margin: calc(var(--size) / -2) -1px;
+	}
+
+	.handle.horizontal {
+		--size: 8px;
+		width: var(--size);
+		height: calc(100% + 2px);
+		margin: -1px calc(var(--size) / -2);
+	}
+
+	:global(:hover) > .handle.vertical,
+	:global(:hover) > .handle.horizontal,
+	.handle.vertical.dragging,
+	.handle.horizontal.dragging {
+		--size: var(--cell-size, 24px);
+		width: var(--cell-size, 24px);
+		height: var(--cell-size, 24px);
+
+		border-radius: 50%;
+
+		color: var(--black);
+
+		--x: calc(var(--cell-size, 24px) - 3px);
+		text-align: center;
+		font-size: calc(var(--x, 24px) * 0.5);
+		line-height: var(--cell-size, 24px);
 	}
 </style>
