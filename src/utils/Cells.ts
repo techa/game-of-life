d72ad90d00cec3d0887e7991f0d0c7aaa68b251f
techa <techa27@gmail.com>
2023-11-19 18:35:30 +0900

    refactor:(Cells): Add `DataTemp`

--- src/utils/Cells.ts
+++ src/utils/Cells.ts
@@ -38,13 +38,18 @@ export interface MiniData {
 	d: string
 }
 
-type Bit = 1 | 2 | 4 | 8
+/**
+ * * `x-y:36radixNumber` or `x-y-Base64String`
+ */
+export type DataTemp = `${number}-${number}${':' | '-'}${string}`
+
+export type DataBit = 1 | 2 | 4 | 8
 /**
  *
  * @param strBinary
  * @param bit 1,2,4,8
  */
-export function strBinaryToBase64(strBinary: string, bit: Bit = 1) {
+export function strBinaryToBase64(strBinary: string, bit: DataBit = 1) {
 	const digit = 8 / bit
 	const arr = new Uint8Array(Math.ceil(strBinary.length / digit))
 	let i = 0
@@ -57,7 +62,7 @@ export function strBinaryToBase64(strBinary: string, bit: Bit = 1) {
 	return base64EncArr(arr)
 }
 
-export function base64ToStrBinary(base64: string, bit: Bit = 1) {
+export function base64ToStrBinary(base64: string, bit: DataBit = 1) {
 	return base64DecToArr(base64).reduce(
 		(str, v) => str + v.toString(2 * bit).padStart(8 / bit, '0'),
 		'',
@@ -84,6 +89,22 @@ export class Cells extends Array2d<Cell> {
 		return data
 	}
 
+	decompress({ w, h, b, d }: MiniData) {
+		this.values = (
+			b
+				? parseInt(d, 36)
+						.toString(2)
+						.padStart(w * h, '0')
+				: base64ToStrBinary(d)
+		)
+			.slice(0, w * h)
+			.split('')
+			.map((v) => +v)
+		this.columns = w
+		this.rows = h
+		return this
+	}
+
 	/**
 	 *
 	 * @param bit
@@ -92,7 +113,7 @@ export class Cells extends Array2d<Cell> {
 	 * 8. = cells table state (Generation:2~253)
 	 * @returns "x-y:36radix" or "x-y-Base64DataString"
 	 */
-	encode(bit: Bit = 2) {
+	encode(bit: DataBit = 2): DataTemp {
 		const binary = this.values.map((v) => (bit > 1 ? v + 2 : v)).join('')
 		const base64 = strBinaryToBase64(binary, bit)
 		const d10 = parseInt(binary, 2 ** bit)
@@ -107,41 +128,30 @@ export class Cells extends Array2d<Cell> {
 		}`
 	}
 
-	decode(str: string, bit: Bit = 2) {
-		const b = !!str.includes(':')
-		const [w, h, d] = str.split(/[:-]/).map((v) => +v || v) as [
-			number,
-			number,
-			string,
-		]
-		this.values = (
-			b
-				? parseInt(d, 36)
-						.toString(2 ** bit)
-						.padStart(w * h, '0')
-				: base64ToStrBinary(d, bit)
-		)
-			.slice(0, w * h)
-			.split('')
-			.map((v) => (bit > 1 ? +v - 2 : +v))
-		this.columns = w
-		this.rows = h
-		return this
-	}
+	decode(str: string, bit: DataBit = 2) {
+		try {
+			const b = !!str.includes(':')
+			const [w, h, d] = str.split(/[:-]/).map((v) => +v || v) as [
+				number,
+				number,
+				string,
+			]
+			this.values = (
+				b
+					? parseInt(d, 36)
+							.toString(2 ** bit)
+							.padStart(w * h, '0')
+					: base64ToStrBinary(d, bit)
+			)
+				.slice(0, w * h)
+				.split('')
+				.map((v) => (bit > 1 ? +v - 2 : +v))
+			this.columns = w
+			this.rows = h
+		} catch (err) {
+			console.error(err)
+		}
 
-	decompress({ w, h, b, d }: MiniData) {
-		this.values = (
-			b
-				? parseInt(d, 36)
-						.toString(2)
-						.padStart(w * h, '0')
-				: base64ToStrBinary(d)
-		)
-			.slice(0, w * h)
-			.split('')
-			.map((v) => +v)
-		this.columns = w
-		this.rows = h
 		return this
 	}
 }
