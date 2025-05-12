c421ea45f86b7929c4c844c697f909e10a2107bb
techa <techa27@gmail.com>
2025-05-12 20:19:10 +0900

    chore: update

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import adapter from '@sveltejs/adapter-static'
 // import adapter from '@sveltejs/adapter-auto'
-import preprocess from 'svelte-preprocess'
+// import { sveltePreprocess } from 'svelte-preprocess'
 
 import { readFileSync } from 'fs'
 const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
@@ -12,7 +12,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), vitePreprocess({})],
+	preprocess: [/* sveltePreprocess(),  */ vitePreprocess({})],
 
 	kit: {
 		// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
